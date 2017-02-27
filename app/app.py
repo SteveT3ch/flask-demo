f097d69389ce62bdad78192c7f109dad70851cbd
Steve Peters <steve.t3ch@gmail.com>
2017-02-27 00:02:37 -0500

    Add Debugging

--- app/app.py
+++ app/app.py
@@ -2,10 +2,19 @@ from flask import Flask, request, render_template
 import redis
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
 def mainpage():
     return render_template('index.html')
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return "User " + str(username)
+
+@app.route('/post/<int:post_id')
+def show_post(post_id):
+    return "Post " + post_id
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')

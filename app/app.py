2862ba20a09807a662dffcb994795ea47ec77dd5
Steve Peters <steve.t3ch@gmail.com>
2017-02-27 00:43:32 -0500

    fix debugger

--- app/app.py
+++ app/app.py
@@ -12,9 +12,9 @@ def mainpage():
 def show_user_profile(username):
     return "User " + str(username)
 
-@app.route('/post/<int:post_id')
+@app.route('/post/<int:post_id>')
 def show_post(post_id):
-    return "Post " + post_id
+    return "Post " + str(post_id)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
